b6177149c3a3ae565ca634a8d9ba22de76035e1a
Jeff Culverhouse <jeff@graystorm.com>
2021-01-03 16:11:52 +0000

    AoC 2020 #1 with help from https://fasterthanli.me/series/advent-of-code-2020/part-1

--- advent01/src/main.rs
+++ advent01/src/main.rs
@@ -1,47 +1,31 @@
-use std::fs::File;
-use std::io::{BufRead, BufReader, Error, ErrorKind, Read};
+use itertools::Itertools;
 
-fn read<R: Read>(io: R) -> Result<Vec<i64>, Error> {
-    let br = BufReader::new(io);
-    br.lines()
-        .map(|line| line.and_then(|v| v.parse().map_err(|e| Error::new(ErrorKind::InvalidData, e))))
-        .collect()
-}
+fn main() -> anyhow::Result<()> {
+    let (a, b) = include_str!("input.txt")
+        .lines()
+        .map(str::parse::<i64>)
+        .collect::<Result<Vec<_>, _>>()?
+        .into_iter()
+        .tuple_combinations()
+        .find(|(a, b)| a + b == 2020)
+        .expect("no pair had a sum of 2020");
 
-fn main() {
-    let nums = read(File::open("input").unwrap()).unwrap();
-    part1(&nums);
-    part2(&nums);
-}
+    println!("Part 1:");
+    dbg!(a + b);
+    dbg!(a * b);
 
-fn part1(nums: &Vec<i64>) {
-    println!("\nPart 1:");
-    for num1 in nums.iter() {
-        for num2 in nums.iter() {
-            if num1 + num2 == 2020 {
-                println!("{} + {} == 2020, multiplied = {}", num1, num2, num1 * num2);
-                return;
-            }
-        }
-    }
-}
+    let (a, b, c) = include_str!("input.txt")
+        .lines()
+        .map(str::parse::<i64>)
+        .collect::<Result<Vec<_>, _>>()?
+        .into_iter()
+        .tuple_combinations()
+        .find(|(a, b, c)| a + b + c == 2020)
+        .expect("no tuple of length 3 had a sum of 2020");
 
-fn part2(nums: &Vec<i64>) {
-    println!("\nPart 2:");
-    for num1 in nums.iter() {
-        for num2 in nums.iter() {
-            for num3 in nums.iter() {
-                if num1 != num2 && num1 != num3 && num2 != num3 && num1 + num2 + num3 == 2020 {
-                    println!(
-                        "{} + {} + {} == 2020, multiplied = {}",
-                        num1,
-                        num2,
-                        num3,
-                        num1 * num2 * num3
-                    );
-                    return;
-                }
-            }
-        }
-    }
+    println!("Part 2:");
+    dbg!(a + b + c);
+    dbg!(a * b * c);
+
+    Ok(())
 }
